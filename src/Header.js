2374da56e26a238ab9a8b1f949ab9499b94bdaba
Gamingapple <madhikarmianshu@gmail.com>
2023-02-19 16:31:16 +1100

    logo not showing

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ function Header(){
     return(
         <div className="header">
             <div className="logo-and-title">
-                <img src="/favicon.ico" className="logo"/>
+                <img src="/favicon.ico?" className="logo"/>
                 <div className="logo-title">Meme Generator</div>
             </div>
             <div className="project-name">
