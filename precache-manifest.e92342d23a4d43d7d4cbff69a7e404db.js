ed8a91d0f8de715adf4236a50db220f4cbfa6b41
Gamingapple <madhikarmianshu@gmail.com>
2023-02-19 15:21:55 +1100

    Updates

---
+++ precache-manifest.e92342d23a4d43d7d4cbff69a7e404db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfa7ad07e5c18dd72489",
+    "url": "/memegenerator/static/css/main.b6fb4a05.chunk.css"
+  },
+  {
+    "revision": "bfa7ad07e5c18dd72489",
+    "url": "/memegenerator/static/js/main.bfa7ad07.chunk.js"
+  },
+  {
+    "revision": "54087e177e7c5a193df0",
+    "url": "/memegenerator/static/js/1.54087e17.chunk.js"
+  },
+  {
+    "revision": "ac4c11ca53d7e93e623b",
+    "url": "/memegenerator/static/js/2.ac4c11ca.chunk.js"
+  },
+  {
+    "revision": "41fec805533842c83011",
+    "url": "/memegenerator/static/js/runtime~main.41fec805.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/memegenerator/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "53201ac794706e41b770465bd1e0beb5",
+    "url": "/memegenerator/index.html"
+  }
+];
