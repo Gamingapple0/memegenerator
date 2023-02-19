0fb1bad20a819026a82b45090f853416004c48a6
Gamingapple <madhikarmianshu@gmail.com>
2023-02-19 16:36:10 +1100

    Added Favicon in header

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ function Header(){
     return(
         <div className="header">
             <div className="logo-and-title">
-                <img src="/favicon.ico?" className="logo"/>
+                <img src="https://github.com/Gamingapple0/memegenerator/blob/gh-pages/favicon.ico?raw=true" className="logo"/>
                 <div className="logo-title">Meme Generator</div>
             </div>
             <div className="project-name">
